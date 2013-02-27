4232bc308d52223193536bc7c2d63e48a495b984
Adrian Jarvis <adrian.jarvis@clear.net.nz>
2013-02-27 19:35:14 +1300

    Fixed the layout to display the title text

--- src/wordgame/WordGame.java
+++ src/wordgame/WordGame.java
@@ -1,14 +1,19 @@
 package wordgame;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -54,9 +59,16 @@ public class WordGame extends Application {
         }
         
         primaryStage.setScene(new Scene(root));
-        Label titleLabel = new Label("Ryan's Word Game");
-        root.getChildren().add(titleLabel);
-        root.getChildren().add(tilePane);
+        Text titleLabel = new Text("Ryan's Word Game");
+        titleLabel.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 24.));
+        titleLabel.setFill(Color.CADETBLUE);
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(10));
+        vBox.setSpacing(8.);
+        vBox.getChildren().add(titleLabel);
+        vBox.getChildren().add(tilePane);
+        root.getChildren().add(vBox);
         primaryStage.show();
     }
 }
